d60c56f6dca1d0aa1e10461a96944a2bcade79b5
Dylan Gutierrez <dylangut@uoregon.edu>
2026-01-21 13:16:59 -0800

    Created a "java buid" and it took forever to set up

---
+++ lstore/java-db/src/main/java/com/dylangutierrez/lstore/App.java
@@ -0,0 +1,13 @@
+package com.dylangutierrez.lstore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ lstore/java-db/src/test/java/com/dylangutierrez/lstore/AppTest.java
@@ -0,0 +1,16 @@
+package com.dylangutierrez.lstore;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
