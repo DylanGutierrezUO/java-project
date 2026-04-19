d89a154cc68379530c36cd7680fe973edd6248a4
Dylan Gutierrez <dylangut@uoregon.edu>
2026-04-19 12:36:01 -0700

    Adjusting for Spring Boot

---
+++ java-db/src/main/java/com/dylangutierrez/lstore/JavaDbApplication.java
@@ -0,0 +1,15 @@
+package com.dylangutierrez.lstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main Spring Boot application entry point for the demo website.
+ */
+@SpringBootApplication
+public class JavaDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaDbApplication.class, args);
+    }
+}
